caba208cca8a8dd0399ab7a9c388114407aa73a5
okarlyuc <snownoop@gmail.com>
2016-02-23 01:12:56 +0200

    create initial import

---
+++ index.js
@@ -0,0 +1,21 @@
+module.exports = {
+  "parserOptions": {
+    "parser": "babel-eslint",
+    "ecmaVersion": 6,
+    "sourceType": "module"
+  },
+  "rules": {
+    "quotes": 0,
+    "comma-dangle": 1,
+    "no-trailing-spaces": 0,
+    "eol-last": 0,
+    "no-unused-vars": 0,
+    "no-underscore-dangle": 0,
+    "no-alert": 0,
+    "no-debugger": 0,
+    "no-lone-blocks": 0,
+    "new-cap": 0,
+    "space-before-blocks": 1,
+    "semi": [1, "always"]
+  }
+};
